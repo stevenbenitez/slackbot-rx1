28c4dad946f3f8a9d6b7c5790598d81903439a0b
Steven Benitez <benitezs@infinity-software.com>
2015-06-14 01:54:05 -0400

    Initial commit for Slackbot RX-1

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+require './slackbot.rb'
+run Sinatra::Application
---
+++ slackbot.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'json'
+
+# token=XXXXXXXXXXXXXXXXXX
+# team_id=T0001
+# team_domain=example
+# channel_id=C2147483705
+# channel_name=test
+# timestamp=1355517523.000005
+# user_id=U2147483697
+# user_name=Steve
+# text=googlebot: What is the air-speed velocity of an unladen swallow?
+# trigger_word=googlebot:
+
+get '/' do
+  token = params['token']
+  if token != 'n4CZSKfZqpO48YOLe0jW6HMH'
+    status 403
+    return ''
+  end
+
+
+  team_id = params['team_id']
+  team_domain = params['team_domain']
+  channel_id = params['channel_id']
+  channel_name = params['channel_name']
+  timestamp = params['timestamp']
+  user_id = params['user_id']
+  user_name = params['user_name']
+  text = params['text']
+  trigger_word = params['trigger_word']
+
+  response = "Hi, @#{user_name}!"
+
+  content_type 'application/json'
+  JSON.generate({ :text => response })
+end
